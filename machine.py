072685752cd57f4216165f508f14ad90b979099a
Erik Nomitch <enomitch@gmail.com>
2011-08-05 10:17:44 -0700

    initial; with files

---
+++ machine.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+# ------------------------------------------------
+# IMPORT -----------------------------------------
+# ------------------------------------------------
+import os
+import sys
+import json
+
+# ------------------------------------------------
+# CLASS->MACHINE ---------------------------------
+# ------------------------------------------------
+class Machine:
+    def __init__(self, path_json_config):
+        self.json_config = open(path_json_config)
+        self.json_config = json.load(self.json_config)
+        print self.json_config["packages"]
+
+    def ensure_system_updated(self):
+        return
+
+    def install_packages(self):
+        return
+    
+    def install_package(self, package):
+        return
+
+# ------------------------------------------------
+# GLOBALS ----------------------------------------
+# ------------------------------------------------
+machine = Machine("machine.json")
+
+# ------------------------------------------------
+# MAIN -------------------------------------------
+# ------------------------------------------------
+def main():
+    if os.getenv("USER") != "root":
+        #print "fatal: You need to be root."
+        #sys.exit(1)
+        print "skipping root check"
+    sys.exit(0)
+
